ce9e1d885bbde6cc5823e6aacbd2773d280295d8
flippette <me@flippo.cc>
2025-08-15 11:53:34 +0700

    easy#13: move value translation into atom function

--- src/easy/lc13.rs
+++ src/easy/lc13.rs
@@ -5,37 +5,8 @@ pub fn roman_to_int(s: impl AsRef<str>) -> u16 {
   let mut n = 0;
 
   while let Some((atom, rest)) = atom(s) {
-    let frag = match atom {
-      "MMM" => 3000,
-      "CCC" => 300,
-      "XXX" => 30,
-      "III" => 3,
-
-      "MM" => 2000,
-      "CC" => 200,
-      "XX" => 20,
-      "II" => 2,
-
-      "CM" => 900,
-      "CD" => 400,
-      "XC" => 90,
-      "XL" => 40,
-      "IX" => 9,
-      "IV" => 4,
-
-      "M" => 1000,
-      "D" => 500,
-      "C" => 100,
-      "L" => 50,
-      "X" => 10,
-      "V" => 5,
-      "I" => 1,
-
-      other => unreachable!("unknown atom: {other}"),
-    };
-
     s = rest;
-    n += frag;
+    n += atom;
   }
 
   n
@@ -49,18 +20,22 @@ fn tag<'a>(s: &'a str, tag: &str) -> Option<(&'a str, &'a str)> {
   }
 }
 
-fn atom(s: &str) -> Option<(&str, &str)> {
+fn atom(s: &str) -> Option<(u16, &str)> {
   #[rustfmt::skip]
-  const ATOMS: [&str; 21] = [
-    "MMM", "CCC", "XXX", "III",
-    "MM", "CC", "XX", "II",
-    "CM", "CD", "XC", "XL", "IX", "IV",
-    "M", "D", "C", "L", "X", "V", "I",
+  const ATOMS: [(&str, u16); 21] = [
+    ("MMM", 3000), ("CCC", 300), ("XXX",  30), ("III",  3),
+
+    ( "MM", 2000), ( "CC", 200), ( "XX",  20), ( "II",  2),
+    ( "CM",  900), ( "CD", 400), ( "XC",  90), ( "XL", 40),
+    ( "IX",    9), ( "IV",   4),
+
+    (  "M", 1000), (  "D", 500), (  "C", 100), (  "L", 50),
+    (  "X",   10), (  "V",   5), (  "I",   1),
   ];
 
-  for atom in ATOMS {
-    if let some @ Some(_) = tag(s, atom) {
-      return some;
+  for (key, val) in ATOMS {
+    if let Some((_, rest)) = tag(s, key) {
+      return Some((val, rest));
     }
   }
 
